22a6a5d320c80937ffe663f914058500b955e73f
lakshmi <lakshmi.bhumagar@gmail.com>
2016-02-29 14:41:39 +0530

    First commit

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,15 +1,67 @@
-//============================================================================
-// Name        : lab2.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include<fstream>
+#include<stdio.h>
+#include<stdlib.h>
 using namespace std;
+class student
+{
+	string  usn,name,sem;
+public:
+	void read();
+	void pack();
+	void display();
+	void unpack();
+	void search();
+};
+fstream fp;
+void student::read(void)
+{
+	cout<<"enter usn,name,sem"<<endl;
+	cin>>usn;
+	cin>>name;
+	cin>>sem;
+}
+void student::pack(void)
+{
+	string buffer;
+	buffer=usn+"|"+name+"|"+sem;
+	buffer.resize(100,'$');
+	fp<<buffer<<endl;
+}
+void student::unpack()
+{
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+}
+
+void student::search()
+{
+
+}
+int main()
+{
+	int choice;
+	student s;
+	while(1)
+	{
+	cout<<"1.read data 2.search"<<endl;
+	cout<<"enter ua choice"<<endl;
+	cin>>choice;
+
+	switch(choice)
+	{
+	case 1:s.read();
+		fp.open("file1.txt",ios::out|ios::app);
+		if(!fp)
+		cout<<"not open"<<endl;
+		s.pack();
+		fp.close();
+		break;
+	case 2:s.search();
+			break;
+	default: exit(0);
+	break;
+	}
+
+	}
 	return 0;
 }
